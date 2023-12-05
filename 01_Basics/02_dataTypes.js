b8c1ea5ba29bcfb4ad831b168f1994797958bb6d
RadheyAmbre <142075435+RadheyAmbre@users.noreply.github.com>
2023-12-05 05:11:47 +0000

    container config done

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // Treat all JS code as newer version
+
+// alert(3+3); // we are using node.js, not browser
+
+console.log(3
+    +
+    3); // code readability should be high
+
+console.log("Radhey");
+
+let name = "radhey"
+let age = 23
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// String => ""/''
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
