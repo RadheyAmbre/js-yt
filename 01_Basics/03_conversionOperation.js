53c56ce8163cfa865e6ce75416a115f7d66baebd
RadheyAmbre <142075435+RadheyAmbre@users.noreply.github.com>
2023-12-06 13:40:15 +0000

    converstion of dataTypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "23rara"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33" => Not a Number NaN
+// true => 1; false => 0
+// null => 0
+// undefined => NaN
+
+let isLoggedIn = 0
+
+let boleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(boleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// " " => false
+// "radhey" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
