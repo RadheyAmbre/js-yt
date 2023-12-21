a831bb28c4334184eebba4aa54dfbbde7558edf2
RadheyAmbre <142075435+RadheyAmbre@users.noreply.github.com>
2023-12-21 12:57:11 +0000

    Container done

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// this kind of comparision should be avoided
+// console.log(null > 0);
+// console.log(null >= 0);
+// console.log(null == 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// === - strickly check the values very throughly
+// check the data type too.
+
+// console.log("2" == 2);   // true
+// console.log("2" === 2);  // false
