e15fc7b8c51b23bbab45e23f49bfef562133091a
RadheyAmbre <142075435+RadheyAmbre@users.noreply.github.com>
2023-12-23 16:43:59 +0000

    math n num

--- 01_basics/06_nums_n_math.js
+++ 01_basics/06_nums_n_math.js
@@ -13,6 +13,31 @@ const otherNum1 = 23.8922
 const otherNum2 = 123.8922
 const otherNum3 = 1234.8922
 
-console.log(otherNum1.toPrecision(3));
-console.log(otherNum2.toPrecision(3));
-console.log(otherNum3.toPrecision(3));
+// console.log(otherNum1.toPrecision(3));
+// console.log(otherNum2.toPrecision(3));
+// console.log(otherNum3.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// -------------------------------------------------
+
+// console.log(Math);
+// get values from 0 to 1
+// console.log(Math.abs(-4));
+// convert negtive to positive & keep positive to positive
+// console.log(Math.round(4.6));   // round-off
+// console.log(Math.ceil(4.3));   // top values
+// console.log(Math.floor(4.9));   // bottom values
+// console.log(Math.min(4.9, 4.7, 4.1, 4.45));
+// min values
+// console.log(Math.max(4.9, 4.7, 4.1, 4.45));
+// max values
+
+// console.log(Math.random());
+// console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
