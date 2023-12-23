d38f98923bbf6966f10e4635140dee3aeb4681b4
RadheyAmbre <142075435+RadheyAmbre@users.noreply.github.com>
2023-12-23 13:30:06 +0000

    incomplete commit

---
+++ 01_basics/06_nums_n_math.js
@@ -0,0 +1,18 @@
+const score = 100
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+const otherNum1 = 23.8922
+const otherNum2 = 123.8922
+const otherNum3 = 1234.8922
+
+console.log(otherNum1.toPrecision(3));
+console.log(otherNum2.toPrecision(3));
+console.log(otherNum3.toPrecision(3));
