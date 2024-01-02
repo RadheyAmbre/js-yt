63a4b5faaf27fe427eb8a05a6a332f6a45739cea
RadheyAmbre <142075435+RadheyAmbre@users.noreply.github.com>
2024-01-02 16:26:30 +0000

    arrow done

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -5,12 +5,43 @@ const user = {
 
   welcomeMessage: function () {
     console.log(`${this.username}, welcome to website`);
-    console.log(this);  // this is talikng about current context init.
+    // console.log(this);  // this is talikng about current context init.
   },
 };
 // user.welcomeMessage();
 // user.username = "harsh";
 // user.welcomeMessage();
-console.log(this);
+
+// console.log(this);
 // it will give empty object{}
 // when we log this in console. It will give window object
+
+// function chai() {
+//   let username = "radhey";
+//   console.log(this.username); // undefined
+// }
+
+// const chai = function () {
+//   let username = "radhey";
+//   console.log(this.username); // undefined
+// };
+
+// const chai = () => {
+//   let username = "radhey";
+//   console.log(this.username); // undefined
+// };
+
+// chai();
+
+
+// const addtwo = (num1, num2) => {
+//   return num1+num2;  // Explicit return
+// }
+
+// const addtwo = (num1, num2) => num1+num2;  // Implicit return
+
+// const addtwo = (num1, num2) => (num1+num2); // Implicit return
+
+const addtwo = (num1, num2) => ({username: "radhey"});
+
+console.log(addtwo(3, 2));
