f2cb0d8e0a148f2fc1abe14f226977659d82eb8e
RadheyAmbre <radheyambre0@gmail.com>
2024-01-31 20:57:38 +0530

    changes in vs code

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,24 +1,22 @@
- const accountId = 144553
- let accountEmail = "ra@gmail.com"
- var accountPassword = "12345"
- accountCity = "Mumbai" 
- let accountState;
+const accountId = 144553
+let accountEmail = "ra@gmail.com"
+var accountPassword = "12345"
+accountCity = "Mumbai"
+let accountState;
 //  accountId = 2  // not allowed
 
 accountEmail = "rdx@yahoo.com"
 
-accountPassword = 
-"789123"
+accountPassword =
+    "789123"
 
 accountCity = "Delhi"
 
- console.log(accountId);
+console.log(accountId);
 
 /*
 Prefer not to use var
 because of issue in block scope and functional scope
 */
 
- console.table([accountId, accountEmail, accountPassword,
- accountCity,
- accountState])
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
