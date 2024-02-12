86f44eb49e847e58a4801e7a8243cbc102beda95
RadheyAmbre <radheyambre0@gmail.com>
2024-02-12 07:22:12 +0530

    intermidiate started

---
+++ 04_control_flow/one.js
@@ -0,0 +1,29 @@
+// if 
+
+// const isUserLoggedIn = true
+// const temperature = 41
+// if (temperature === 41) {
+//     console.log("less than 50");
+// }
+// else {
+//     console.log("temperature is greater than 50");
+// }
+// console.log("Execute");
+
+// <, >, <=, >=, ==, !=, ===, !==
+// are used for comparison. They return a boolean value: true or false
+
+
+// const score = 200
+// if (score > 100) {
+//     let power = "Fly";
+//     console.log(`User power: ${power}`);
+// }
+// console.log(`User power: ${power}`);    // It is not accessible outside of the if block
+
+
+// Implicit Scope
+const balance = 1000
+// if (balance > 500) console.log("test"), console.log("test2");   
+// Multiple statements can be written in one line separated by commas
+// but we dont use this kind of code.
