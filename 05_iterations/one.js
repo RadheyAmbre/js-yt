735fb2e5bce62edbbfb44048c906ddcf4f2e8e85
RadheyAmbre <radheyambre0@gmail.com>
2024-02-13 22:22:20 +0530

    for loop completed

---
+++ 05_iterations/one.js
@@ -0,0 +1,41 @@
+// for
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     if (element == 5) {
+//         console.log("reached the middle");
+//     }
+//     console.log(element);
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     // console.log(`Outer loop value: ${i}`);
+//     for (let j = 1; j <= 10; j++) {
+//         // console.log(`Outer loop value: ${j}`);
+//         // console.log(i, '*', j ,' = ', i * j);
+//     }
+// }
+
+// let myArray = ["ace", "sabo", "luffy"]
+// for (let i = 0; i < myArray.length; i++) {
+//     const element = myArray[i];
+//     console.log(element);
+// }
+
+// break and continue
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i == 5) {
+//         console.log(`Detected 5`);
+//         break;
+//     }
+//     console.log(`Value of i is ${i}`);
+// }
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i == 5) {
+//         console.log(`Detected 5`);
+//         continue;
+//     }
+//     console.log(`Value of i is ${i}`);
+// }
