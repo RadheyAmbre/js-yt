d1ef41ae55144325e9ceb21a33e29a388ea6119d
RadheyAmbre <radheyambre0@gmail.com>
2024-02-15 15:09:58 +0530

    reduce completed

---
+++ 05_iterations/eigth.js
@@ -0,0 +1,36 @@
+const myNums = [1, 2, 3, 4, 5]
+
+// const myTotal = myNums.reduce(function (prev, curr) {
+//     console.log(`prev: ${prev} and curr ${curr}`);
+//     return prev = curr;
+// }, 0);
+
+// const myTotal = myNums.reduce((prev, curr) => prev + curr, 0)
+// console.log(myTotal);
+
+
+const shoppingCart = [
+    {
+        itemName: "Advance Excel and Tally",
+        price: 15000
+    },
+    {
+        itemName: "Full Stack",
+        price: 45000
+    },
+    {
+        itemName: "Full Stack Python",
+        price: 99999
+    },
+    {
+        itemName: "Full Stack Java",
+        price: 124999
+    },
+    {
+        itemName: "Data Science",
+        price: 149999
+    }
+]
+
+// const priceToPay = shoppingCart.reduce((prev, item) => prev + item.price, 0);
+// console.log(priceToPay);
