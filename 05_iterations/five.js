34688b491b10a12dc629b5626be7f7e17e5f9c6c
RadheyAmbre <radheyambre0@gmail.com>
2024-02-15 13:04:23 +0530

    forEach completed

---
+++ 05_iterations/five.js
@@ -0,0 +1,42 @@
+const code = ['js', 'python', 'cpp', 'ruby', 'r']
+
+
+// code.forEach(function (val) {
+//     console.log(val);
+// });
+
+
+// code.forEach((item) => {
+//     console.log(item);
+// });
+
+
+// function printMe(i) {
+//     console.log(i);
+// }
+// code.forEach(printMe)
+
+
+// code.forEach((items, index, arr) => {
+//     console.log(items, index, arr);
+// });
+
+
+const myCoding = [
+    {
+        langName: "Javascript",
+        langFileName: "js"
+    },
+    {
+        langName: "Python",
+        langFileName: "py"
+    },
+    {
+        langName: "C++",
+        langFileName: "cpp"
+    }
+]
+
+// myCoding.forEach((item) => {
+//     console.log(item.langName + ": " + item.langFileName);
+// });
