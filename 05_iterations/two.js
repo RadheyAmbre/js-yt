384c836d1625e7911fc1f732a553309ca374514e
RadheyAmbre <radheyambre0@gmail.com>
2024-02-15 10:28:38 +0530

    while n dowhile completed

---
+++ 05_iterations/two.js
@@ -0,0 +1,21 @@
+// While loop
+// let i = 0;
+// while (i <= 10) {
+//     console.log(`Index value is ${i}`);
+//     i = i + 2;
+// }
+
+// let myArray = ['ace', 'sabo', 'luffy']
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr++;
+// }
+
+
+// Do While loop
+// let score = 1
+// do {
+//     console.log(`Score is ${score}`);
+//     score++;
+// } while (score <= 10);
